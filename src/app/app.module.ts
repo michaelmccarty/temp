f839dd0db604c2fb81790f6d9750a2b234947044
Michael McCarty <michael@lemuria.local>
2019-11-19 23:54:53 -0500

    Alpha test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewcompComponent } from './newcomp/newcomp.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, NewcompComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
